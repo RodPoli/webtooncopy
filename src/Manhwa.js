6bb44527a2520f8d4338a39047ed6c6a3e288b84
RodPoli <142408199+RodPoli@users.noreply.github.com>
2024-02-19 15:23:03 +0800

    Added for conditional rendering

--- src/Manhwa.js
+++ src/Manhwa.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-
+import { faCrown } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
 function Manhwa(props) {
   const { photoName, name, author, likes } = props.manhwaObj;
 
@@ -19,9 +21,36 @@ function Manhwa(props) {
           <h3
             style={{ color: "#01cc62", marginLeft: "5px", marginBottom: "0" }}
           >
-            {likes}
+            {likes} M
           </h3>
         </div>
+      </div>
+      <div className="condition">
+        {likes > 30 ? (
+          <h3 className="golden">
+            <FontAwesomeIcon icon={faCrown} style={{ color: "#ffd700" }} />
+            Golden Award!
+          </h3>
+        ) : (
+          ""
+        )}
+        {likes > 10 ? (
+          <h3 className="best">
+            {" "}
+            <FontAwesomeIcon icon={faTrophy} style={{ color: "#f11a25" }} />
+            Best of the best!
+          </h3>
+        ) : (
+          ""
+        )}
+        {likes > 5 ? (
+          <h3 className="choice">
+            <FontAwesomeIcon icon={faCheck} style={{ color: "#01cc62" }} />
+            Reader's Choice!
+          </h3>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
